b402b27894fe7c64aa57bbf722244da92cff3512
Paul Craggs <craggs@gmail.com>
2021-12-05 11:39:05 +0000

    day 5

---
+++ 2021/day5/task1.ts
@@ -0,0 +1,66 @@
+
+{
+    const input = await Deno.readTextFile('input.txt');
+
+    const X = 0;
+    const Y = 1;
+
+    const coords = input.split('\n').map(s => 
+    {
+        const parts = s.split(' -> ');
+        return {
+            start: parts[0].split(',').map(Number),
+            finish: parts[1].split(',').map(Number)
+        }
+    });
+
+    const filteredCoords = coords; // coords.filter(c => c.start[X] === c.finish[X] || c.start[Y] === c.finish[Y]);
+
+    const populatedPoints: { [id: string] : number; } = {};
+
+    for (let line of filteredCoords) {
+
+        let x = line.start[X];
+        let y = line.start[Y];
+
+        let key = `${x},${y}`;
+        if (populatedPoints[key] === undefined) {
+            populatedPoints[key] = 0;
+        }
+
+        populatedPoints[key]++;
+
+        while (x != line.finish[X] || y != line.finish[Y])  {
+
+            if (x < line.finish[X]) {
+                x++;
+            } else if (x > line.finish[X]) {
+                x--;
+            }
+            if (y < line.finish[Y]) {
+                y++;
+            } else if (y > line.finish[Y]) {
+                y--;
+            }
+
+            key = `${x},${y}`;
+            if (populatedPoints[key] === undefined) {
+                populatedPoints[key] = 0;
+            }
+
+            populatedPoints[key]++;
+
+        }
+    }
+
+    let multiHitCounter = 0;
+
+    for (let key of Object.keys(populatedPoints)) {
+        if (populatedPoints[key] > 1) {
+            multiHitCounter++;
+        }
+    }
+
+    console.log(multiHitCounter);
+
+}
---
+++ 2021/day5/task2.ts
@@ -0,0 +1,64 @@
+
+{
+    const input = await Deno.readTextFile('input.txt');
+
+    const X = 0;
+    const Y = 1;
+
+    const coords = input.split('\n').map(s => 
+    {
+        const parts = s.split(' -> ');
+        return {
+            start: parts[0].split(',').map(Number),
+            finish: parts[1].split(',').map(Number)
+        }
+    });
+
+    const populatedPoints: { [id: string] : number; } = {};
+
+    for (let line of coords) {
+
+        let x = line.start[X];
+        let y = line.start[Y];
+
+        let key = `${x},${y}`;
+        if (populatedPoints[key] === undefined) {
+            populatedPoints[key] = 0;
+        }
+
+        populatedPoints[key]++;
+
+        while (x != line.finish[X] || y != line.finish[Y])  {
+
+            if (x < line.finish[X]) {
+                x++;
+            } else if (x > line.finish[X]) {
+                x--;
+            }
+            if (y < line.finish[Y]) {
+                y++;
+            } else if (y > line.finish[Y]) {
+                y--;
+            }
+
+            key = `${x},${y}`;
+            if (populatedPoints[key] === undefined) {
+                populatedPoints[key] = 0;
+            }
+
+            populatedPoints[key]++;
+
+        }
+    }
+
+    let multiHitCounter = 0;
+
+    for (let key of Object.keys(populatedPoints)) {
+        if (populatedPoints[key] > 1) {
+            multiHitCounter++;
+        }
+    }
+
+    console.log(multiHitCounter);
+
+}
